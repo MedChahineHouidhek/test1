84404426a40b9a5e911f84aed7f21ce29af843a6
MedChahineHouidhek <chahinehouidhek2@gmail.com>
2023-09-01 15:40:15 -0400

    change

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once "personne.php";
+
+$chahine = new personne();
+
+$chahine->setName("Chahine");
+
+echo $chahine->getName();
+    
+?>
---
+++ personne.php
@@ -0,0 +1,20 @@
+<?php 
+class personne{
+    public $name ; 
+    public $address ; 
+    public $zipcode ; 
+    public $phone ; 
+    public $email ;
+    public function getName(){
+        return $this->name;
+    }
+    public function setName($name){
+        $this->name = $name;
+    }   
+}
+
+
+
+
+
+?>
